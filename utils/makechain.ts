6eac090f825b7ca8ec36fd399033cc85b1b7e222
shawnmccarthy <shawn@mccarthysoftware.com>
2023-05-30 09:37:31 -0600

    remove pinecone

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,23 +2,31 @@ import { OpenAI } from 'langchain/llms/openai';
 import { FaissStore } from "langchain/vectorstores/faiss";
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
+const CONDENSE_PROMPT_NEW = `You are an AI assistant tasked with rephrasing follow-up questions to be standalone questions. Given a conversation history and a follow-up question, your task is to rephrase the follow-up question in a way that it remains relevant and clear even without the context of the previous conversation.
+
+Chat History:
+{chat_history}
+
+Follow-Up Input: {question}
+
+Rephrased standalone question:`;
+
+const QA_PROMPT_NEW: string = `You are an AI assistant designed to provide information on a specific topic. You can generate tests, quizzes, code, and presentations related to that topic, as well as answer questions and provide helpful explanations.
+Your goal is to be as informative and helpful as possible within the scope of your expertise. If you don't know the answer to a question, provide a response that is closely related to the topic.
+Please note that your area of expertise is limited to the specific topic you were designed for. If someone asks a question outside of this topic, politely explain that you are unable to provide an answer.
+When asked to solve math problems or interpret equations, your responses should be formatted in LaTeX (preferably KaTex) syntax. For code snippets, use Markdown syntax with the appropriate language tag (e.g., \`\`\`javascript for JavaScript code). For other queries, use standard Markdown syntax.
+
+Context: {context}
+
+Question: {question}
+Helpful answer (in LaTeX (preferably KaTex) syntax for math, equations, or formulas; in Markdown with language tag for code snippets; otherwise, in standard Markdown syntax):`;
+
 const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
 Chat History:
 {chat_history}
 Follow Up Input: {question}
 Standalone question:`;
-
-const QA_PROMPT_NEW: string = `You are an AI assistant with a specialization in coding, analyzing and interpreting content from various documents contained in the context. These documents range from academic articles to technical reports, with a focus on JavaScript you can generate useful code snippets, including JSON objects, based on the information in these documents 
-Your role is to generate useful explainations, summaries and code snippets based on the information in the context, and to answer questions about the articles themselves. You're also capable of offering explanations and insights related to your area of expertise.
-In the event a question falls outside of your knowledge domain or the context below, kindly inform the user that you are unable to provide an answer.
-When asked to solve math problems or interpret equations, your responses should be formatted in KaTeX syntax. For other queries, use Markdown syntax.
-
-Context: {context}
-
-Question: {question}
-
-Expected answer (in KaTeX for math, equations, or formulas; otherwise, in Markdown syntax):`;
 
 const QA_PROMPT: string = `You are an AI assistant designed to provide information on a specific topic. You can generate tests, quizzes, code, and presentations related to that topic, as well as answer questions and provide helpful explanations.
 Your goal is to be as informative and helpful as possible within the scope of your expertise. If you don't know the answer to a question, provide a response that is closely related to the topic.
@@ -29,26 +37,6 @@ Prompt:
 
 Question: {question}
 Helpful answer in katex or markdown syntax:`;
-
-const QA_PROMPT1: string = `You are an AI assistant designed to provide information on a specific topic. You can generate tests, quizzes, code, and presentations related to that topic, as well as answer questions and provide helpful explanations.
-Your goal is to be as informative and helpful as possible within the scope of your expertise. If you don't know the answer to a question, provide a response that is closely related to the topic.
-Please note that your area of expertise is limited to the specific topic you were designed for. If someone asks a question outside of this topic, politely explain that you are unable to provide an answer.
-When asked to solve math problems or interpret equations, your responses should be formatted in LaTeX (preferably KaTex) syntax. For code snippets, use Markdown syntax with the appropriate language tag (e.g., \`\`\`javascript for JavaScript code). For other queries, use standard Markdown syntax.
-
-Context: {context}
-
-Question: {question}
-Helpful answer (in LaTeX (preferably KaTex) syntax for math, equations, or formulas; in Markdown with language tag for code snippets; otherwise, in standard Markdown syntax):`;
-
-const QA_PROMPT_OLD = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-
-{context}
-
-Question: {question}
-Helpful answer in markdown:`;
-
 
 export const makeChainFaiss = (vectorstore: FaissStore, temp: number = 0.5) => {
   console.log('creating chain with temperature', temp);
@@ -62,8 +50,8 @@ export const makeChainFaiss = (vectorstore: FaissStore, temp: number = 0.5) => {
     model,
     vectorstore.asRetriever(),
     {
-      qaTemplate: QA_PROMPT1,
-      questionGeneratorTemplate: CONDENSE_PROMPT,
+      qaTemplate: QA_PROMPT_NEW,
+      questionGeneratorTemplate: CONDENSE_PROMPT_NEW,
       returnSourceDocuments: true, //The number of source documents returned is 4 by default
     },
   );
